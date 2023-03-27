525e7e0436835e08327778fd524a1cb99ac88a0a
IPL1987 <ihorpoznyak@yandex.com>
2023-03-27 09:35:59 +0300

    fix

--- props/films/src/components/Stars.jsx
+++ props/films/src/components/Stars.jsx
@@ -1,20 +1,14 @@
 import Star from "./Star";
 import PropTypes from 'prop-types'
-import { nanoid } from 'nanoid'
 
-export default function Stars ({count=0}) {
+export default function Stars ({count}) {
   if (typeof count !== 'number' || isNaN(count) || count < 1 || count > 5) {
     return null;
   }
-  const starsItems = [];
-  for (let i = 0; i < count; i++) {
-    starsItems.push(<Star key={nanoid(5)} />)    
-  }
-  console.log(count);
 
   return (
     <ul className="card-body-stars u-clearfix">
-      {starsItems}
+      {Array(count).fill().map((_, count) => <li key={count}><Star /></li>)}
     </ul>
   );
 }
