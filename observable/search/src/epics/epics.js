97548c02614f8000b2acb7a7be31122e7393a73c
IPL1987 <ihorpoznyak@yandex.com>
2023-04-27 17:00:41 +0300

    Init commit

---
+++ observable/search/src/epics/epics.js
@@ -0,0 +1,49 @@
+import { ofType } from "redux-observable";
+import { ajax } from "rxjs/ajax";
+import {
+  map,
+  mergeMap,
+  retry,
+  debounceTime,
+  switchMap,
+  catchError,
+} from "rxjs/operators";
+import {
+  CHANGE_SEARCH_FIELD,
+  SEARCH_SKILLS_REQUEST,
+} from "../actions/actionsType";
+import {
+  searchSkillsRequest,
+  searchSkillsSuccess,
+  searchSkillsFailure,
+  resetSkills,
+} from "../actions/acnionsCreate";
+import { of } from "rxjs";
+
+export const changeSearchEpic = (action$) =>
+  action$.pipe(
+    ofType(CHANGE_SEARCH_FIELD),
+    map((o) => o.payload.search.trim()),
+    debounceTime(100),
+    mergeMap((o) => {
+      if (o === "") {
+        return of(resetSkills());
+      } else {
+        return of(searchSkillsRequest(o));
+      }
+    })
+  );
+
+export const searchSkillsEpic = (action$) =>
+  action$.pipe(
+    ofType(SEARCH_SKILLS_REQUEST),
+    map((o) => o.payload.search),
+    map((o) => new URLSearchParams({ q: o })),
+    switchMap((o) =>
+      ajax.getJSON(`${'http://localhost:7070/api/search'}?${o}`).pipe(
+        retry(3),
+        map((o) => searchSkillsSuccess(o)),
+        catchError((e) => of(searchSkillsFailure(e)))
+      )
+    )
+  );
